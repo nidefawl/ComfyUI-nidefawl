d2662eb8c58c4c87c690487ed1d5fe22dd6219c8
nidefawl <637382+nidefawl@users.noreply.github.com>
2024-01-10 09:45:43 +0100

    Patch torch.nn.modules.module.load_state_dict function.
    - Improves performance and memory overhead (TensorBase copy_() is slower and seams to leak memory)
    - With this patch models store data as fp16 internally

--- __init__.py
+++ __init__.py
@@ -22,16 +22,32 @@ for module_name in node_list:
 WEB_DIRECTORY = "./js"
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "WEB_DIRECTORY"]
 
-import execution
+def patch_default_execute_flush_cache():
+  import execution
 
-def nidefawl_execute_flush_cache(self, *args, **kwargs):
-  if len(args) > 2 and 'extra_pnginfo' in args[2] and 'workflow' in args[2]['extra_pnginfo']:
-    workflow = args[2]['extra_pnginfo']['workflow']
-    is_flush_cache = 'config' in workflow and 'flush_cache' in workflow['config'] and bool(workflow['config']['flush_cache'])
-    if is_flush_cache:
-       self.outputs = {}
-  
-  return self.default_execute(*args, **kwargs)
+  def nidefawl_execute_flush_cache(self, *args, **kwargs):
+    if len(args) > 2 and 'extra_pnginfo' in args[2] and 'workflow' in args[2]['extra_pnginfo']:
+      workflow = args[2]['extra_pnginfo']['workflow']
+      is_flush_cache = 'config' in workflow and 'flush_cache' in workflow['config'] and bool(workflow['config']['flush_cache'])
+      if is_flush_cache:
+        self.outputs = {}
+    
+    return self.default_execute(*args, **kwargs)
 
-execution.PromptExecutor.default_execute = execution.PromptExecutor.execute
-execution.PromptExecutor.execute = nidefawl_execute_flush_cache
+  execution.PromptExecutor.default_execute = execution.PromptExecutor.execute
+  execution.PromptExecutor.execute = nidefawl_execute_flush_cache
+
+def patch_torch_nn_module_load_state_dict():
+  import torch.nn.modules.module
+  from typing import Mapping, Any
+  STATE_DICT_ASSIGN = True
+  def nidefawl_load_state_dict(self, state_dict: Mapping[str, Any],
+                        strict: bool = True, assign: bool = True):
+    assign = STATE_DICT_ASSIGN
+    return self.default_load_state_dict(state_dict, strict, assign)
+
+  torch.nn.modules.module.Module.default_load_state_dict = torch.nn.modules.module.Module.load_state_dict
+  torch.nn.modules.module.Module.load_state_dict = nidefawl_load_state_dict
+
+patch_default_execute_flush_cache()
+patch_torch_nn_module_load_state_dict()
